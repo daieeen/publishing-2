692f98b70947f6390bc2d4676d78b85a50ea89de
daieeen <101250985+daieeen@users.noreply.github.com>
2022-03-30 12:06:02 +0900

    ㅎ

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+     <div>로그인</div>
+     <div>
+     <TextField label='이메일' placeholder="이메일을 입력해주세요." />
+     <TextField label='비밀번호' placeholder="비밀번호를 입력해주세요." />
+     </div>
+     <div></div>
+    </div>
+  );
 };
