fd19db8b2f4f63cf424fd47a161a7183febceb16
daieeen <101250985+daieeen@users.noreply.github.com>
2022-04-06 09:49:50 +0900

    mod 2

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+<div className="mt-4 border border-gray-400">
+  <div className="m-4">
+    <div className="text-3xl font-bold mb-10">New</div>
+    <div className="py-40 bg-gray-200 text-gray-800">
+    </div>
+    <div className="text-xl mt-10">Card title
+     <div className="text-l mb-10 text-gray-400">Sed vel turpis adipiscing penatibus orcineque.
+     Erat sed fermentum ipsum vel quis quam. Nunc eriam dui tortor, non in aliquam lacinia tempor </div>
+    </div>
+    <div className="mt-10">
+       <div className="bg-gray-800 text-white text-center py-4">button</div>
+       <div className="text-center py-4 text-gray-800 border border-gray-800 mt-5">button</div>
+     </div>
+</div>
+
+
+
+
+
+
+
+
+  </div>
+  
+  
+
+
+
+
+
+  );
 };
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+  <div className="m-4">
+    <div className="text-3xl font-bold mb-10">회원가입
+    </div> 
+    <div>
+    <div className="mt-4"><TextField label="아이디" placeholder="아이디를 입력해주세요." /> 
+    </div>
+    <div className="mt-3"><TextField 
+    label="비밀번호" 
+    placeholder="비밀번호를 입력해주세요."
+    type='password' 
+    />
+    </div>
+    <div className="mt-3"><TextField label="이메일" placeholder="이메일을 입력해주세요." />
+    </div>
+    <div className="mt-3"><TextField label="전화번호" placeholder="전화번호를 입력해주세요." />
+    </div>
+  </div> 
+    <div className="mt-10">
+    <div className="text-center py-4 text-gray-800 border border-gray-800 mt-6">회원가입</div>
+    </div>
+   </div>
+  );
 };
